1c2a32c896b82f9e664515b7d7409e677bc35fa8
Tong Zhang <zhangt@frib.msu.edu>
2025-02-13 14:03:46 -0500

    MNT/DM: Clean up.

--- phantasy_apps/data_manager/wfdata/src/wave/convert_tool.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from main import convert_tool
-
-convert_tool()
--- phantasy_apps/data_manager/wfdata/src/wave/plot_tool.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from main import plot_tool
-
-plot_tool()
