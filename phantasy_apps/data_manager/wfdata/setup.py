d588d4c47527a3919cd13b45b55421d71eb2692e
Tong Zhang <zhangt@frib.msu.edu>
2025-02-23 16:56:24 -0500

    MNT/DM: Bump dm.wfdata version to 0.4.2.

--- phantasy_apps/data_manager/wfdata/setup.py
+++ phantasy_apps/data_manager/wfdata/setup.py
@@ -40,7 +40,7 @@ def read_license():
 
 setup(
     name=f'{PKG_NAME}',
-    version='0.4.1',
+    version='0.4.2',
     description='Tools for Managing BPM/BCM waveform data',
     long_description=readme(),
     license=read_license(),
