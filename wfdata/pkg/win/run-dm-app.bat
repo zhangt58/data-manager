d8ebf7cd826ffde7bd8e53363399ec4c3ca08750
Tong Zhang <zhangt@frib.msu.edu>
2025-03-07 22:22:31 -0500

    REF: Update view run cmd for Windows.

--- wfdata/pkg/win/run-dm-app.bat
+++ wfdata/pkg/win/run-dm-app.bat
@@ -8,8 +8,8 @@ set WFDATA_DIR="I:\analysis\linac-data\wfdata"
 %DM_WAVE_CMD% view %WFDATA_DIR%\raw\MPS-faults.csv ^
     %WFDATA_DIR%\final\images ^
     --trip-info-file %WFDATA_DIR%\raw\trip-info.h5 ^
-	--data-dir %WFDATA_DIR%\final\opt ^
-	--data-dir %WFDATA_DIR%\final\merged ^
-	--data-dir %WFDATA_DIR%\raw\raw ^
-	--geometry 1920x1080 ^
-	--fig-dpi 96 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
+    --data-dir %WFDATA_DIR%\final\opt ^
+    --data-dir %WFDATA_DIR%\final\merged ^
+    --data-dir %WFDATA_DIR%\raw\raw ^
+    --geometry 1600x900 --theme arc ^
+    --fig-dpi 96 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
