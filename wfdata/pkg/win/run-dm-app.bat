0fb28087348c3c1b185c98de5793be8b972c705d
Tong Zhang <zhangt@frib.msu.edu>
2025-03-10 16:29:54 -0400

    MNT: Set --fig-dpi 72 by default for Windows.

--- wfdata/pkg/win/run-dm-app.bat
+++ wfdata/pkg/win/run-dm-app.bat
@@ -12,4 +12,4 @@ set WFDATA_DIR="I:\analysis\linac-data\wfdata"
     --data-dir %WFDATA_DIR%\final\merged ^
     --data-dir %WFDATA_DIR%\raw\raw ^
     --geometry 1600x900 --theme arc ^
-    --fig-dpi 96 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
+    --fig-dpi 72 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
