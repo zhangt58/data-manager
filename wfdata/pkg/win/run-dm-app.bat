81b98407a9d6d24cdfc02d659a42429dd72b5d66
Tong Zhang <zhangt@frib.msu.edu>
2025-03-12 15:29:22 -0400

    REF: Start with theme "adapta".

--- wfdata/pkg/win/run-dm-app.bat
+++ wfdata/pkg/win/run-dm-app.bat
@@ -11,5 +11,5 @@ set WFDATA_DIR="I:\analysis\linac-data\wfdata"
     --data-dir %WFDATA_DIR%\final\opt ^
     --data-dir %WFDATA_DIR%\final\merged ^
     --data-dir %WFDATA_DIR%\raw\raw ^
-    --geometry 1600x900 --theme arc ^
+    --geometry 1600x900 --theme adapta ^
     --fig-dpi 72 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
