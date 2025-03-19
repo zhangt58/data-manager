3143ed5e20d5ef1a43230ddc83b688d3e58c0844
Tong Zhang <zhangt@frib.msu.edu>
2025-03-19 17:03:35 -0400

    MNT: Adjust the initial column widths.

--- wfdata/pkg/win/run-dm-app.bat
+++ wfdata/pkg/win/run-dm-app.bat
@@ -12,4 +12,4 @@ set WFDATA_DIR="I:\analysis\linac-data\wfdata"
     --data-dir %WFDATA_DIR%\final\merged ^
     --data-dir %WFDATA_DIR%\raw\raw ^
     --geometry 1600x900 --theme adapta ^
-    --fig-dpi 72 --column-widths "{\"Fault_ID\":80,\"Time\":200,\"Power\":80,\"Destination\":200,\"Ion\":80,\"Type\":80,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
+    --fig-dpi 72 --column-widths "{\"Fault_ID\":80,\"Time\":180,\"Power\":75,\"Destination\":200,\"Ion\":60,\"Type\":70,\"Description\":100,\"Devices\":120,\"T Window\":100,\"Threshold\":100}"
