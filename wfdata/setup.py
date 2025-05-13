3be6c4995dcf671893616c93c4e20e01d3005514
Tong Zhang <zhangt@frib.msu.edu>
2025-05-13 16:39:00 -0400

    MNT: Bump version to 1.2.1.

--- wfdata/setup.py
+++ wfdata/setup.py
@@ -41,7 +41,7 @@ def read_license():
 
 setup(
     name=f'{PKG_NAME}',
-    version='1.2.0',
+    version='1.2.1',
     description='Tools for Managing the post-mortem BPM/BCM waveform data',
     long_description=readme(),
     license=read_license(),
